1606db44aeddfe2c9a273db3c41f118f8badae30
R2Roldan <82239198+R2Roldan@users.noreply.github.com>
2021-10-30 10:30:32 -0500

    archivos nesesarios para crud

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
-from .views import BlogListView
+from .views import BlogDeletView, BlogListView,BlogCreateView, BlogDetailView, BlogUpdateView,DeleteView
 
 
 app_name = "blog"
 urlpatterns = [
-    path('', BlogListView.as_view(), name="home")
+    path('', BlogListView.as_view(), name="home"),
+    path('create/',BlogCreateView.as_view(), name='create'),
+    path('<int:pk>/',BlogDetailView.as_view(),name="detail"),
+    path('<int:pk>/update/',BlogUpdateView.as_view(),name="update"),
+    path('<int:pk>/delete/',BlogDeletView.as_view(),name="delete"),
 ]
